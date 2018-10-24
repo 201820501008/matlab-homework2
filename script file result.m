888ac9aef0e6880f63f4be227bf9ba01ad75e172
201820501008 <44156226+201820501008@users.noreply.github.com>
2018-10-24 15:38:13 +0800

    Create script file result.m

---
+++ script file result.m
@@ -0,0 +1,102 @@
+fid1 =
+
+     9
+
+
+ans =
+
+T
+o
+d
+a
+y
+i
+s
+a
+s
+u
+n
+n
+y
+d
+a
+y
+.
+I
+g
+o
+t
+o
+t
+h
+e
+s
+c
+h
+o
+o
+l
+.
+I
+a
+m
+v
+e
+r
+y
+h
+a
+p
+p
+y
+.
+
+
+fid2 =
+
+     9
+
+
+ans = 
+
+    'Today'
+    'is'
+    'a'
+    'sunny'
+    'day.'
+    'I'
+    'go'
+    'to'
+    'the'
+    'school.'
+    'I'
+    'am'
+    'very'
+    'happy.'
+
+
+fid3 =
+
+     9
+
+
+ans = 
+
+    'Today is a sunny day'
+    'I go to the school'
+    'I am very happy'
+
+
+char_num =
+
+    45
+
+
+sen_num =
+
+     3
+
+
+word_num =
+
+    14
